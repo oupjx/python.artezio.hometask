b29ef4afaf896811345f25bdab38da47c6b91a8c
oupjx <59527865+oupjx@users.noreply.github.com>
2020-01-30 20:38:49 +0300

    Add files via upload

---
+++ Hom2/hom2.py
@@ -0,0 +1,45 @@
+# # 1 задача
+# def my_pow():
+#     a = 0
+#     for i in range(0, 10):
+#         if i % 2 != 0:
+#             print(i*i)
+#             a += 1
+#     print('Количество этих цифр \n',a)
+# my_pow()
+
+
+# # 2 задача
+# def values(a, b, c):
+#     raz = (a-b)//c
+#     print(raz)
+# values(56,23,7)
+
+
+# # 3 задача
+# def factor(n):
+#
+#     if n == 0:
+#         return 1
+#     return factor(n - 1) * n
+#
+# print(factor(7))
+
+# #  5 задача
+# name = input('Как тебя зовут: ')
+# ps = 'admin'
+# while True:
+#     try:
+#         n = input('Введите пароль: ')
+#         if n == ps:
+#             print('Password for user:', name, "is correct")
+#             break
+#         else:
+#             print('Password for user: ', name, 'is incorrect\n','Please try again...')
+#     except:
+#         print("Ошибка - это не пароль")
+
+
+
+
+
